5cd64776bdaa9bb84a9ae4f84f37f18292e10481
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-08-08 15:55:10 +0100

    Verse header for any verse

--- BlankKata.Net/BlankKata.Net/TemplateTest.cs
+++ BlankKata.Net/BlankKata.Net/TemplateTest.cs
@@ -8,9 +8,9 @@ namespace BlankKata.Net;
  * x First line of first verse
  * x First line of second verse
  * x First line of any verse
- * First two lines of first verse
- * First two lines of second verse
- * First two lines of any verse
+ * x First two lines of first verse
+ * x First two lines of second verse
+ * x First two lines of any verse
  * "Gift" line of first verse
  * "Gift" line of second verse
  * "Gift" line of any verse
@@ -56,6 +56,22 @@ public class Tests
         // Assert
         var expected = 
             @"On the first day of Christmas
+My true love sent to me:";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Header_of_any_verse()
+    {
+        // Arrange
+        var song = new ChristmasSong();
+        
+        // Act
+        String result = song.HeaderOf(5);
+        
+        // Assert
+        var expected = 
+            @"On the fifth day of Christmas
 My true love sent to me:";
         Assert.That(result, Is.EqualTo(expected));
     }
